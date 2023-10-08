7ac98a700f68f460356496e8469919d942edc8d3
emrerich <emre.zengin5@ogr.sakarya.edu.tr>
2023-10-08 19:19:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CreateUser from "./pages/createUser/CreateUser";
+import UserList from "./pages/listUser/userList";
+import { DataProvider } from "./pages/listUser/DataContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test-wrapper">
+      <DataProvider>
+        <CreateUser />
+        <UserList />
+      </DataProvider>
     </div>
   );
 }
