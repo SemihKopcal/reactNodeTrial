8d0155ccc0d35d71ad4983f69e19a5c64927b63a
emrerich <emre.zengin5@ogr.sakarya.edu.tr>
2023-10-08 14:14:33 +0300

    commit

--- routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authController = require("../controllers/authController");
-
-const router = express.Router();
-
-router.route("/signup").post(authController.createUser);
-router.route("/all").get(authController.getAllUser);
-
-module.exports = router;
